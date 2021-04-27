56c25ae59c95b9d8512e95c80137790c19175b9c
tianjunfei001 <tianjunfei001@163.com>
2021-04-27 09:49:32 +0800

    gaa 4.27

--- PH7.ERP.Smart.MVC/Controllers/BackstageController.cs
+++ PH7.ERP.Smart.MVC/Controllers/BackstageController.cs
@@ -17,5 +17,120 @@ namespace PH7.ERP.Smart.MVC.Controllers
         {
             return View();
         }
+
+        //医院列表
+        public IActionResult Hospital_list()
+        {
+            return View();
+        }
+
+
+        //科室类型
+        public IActionResult Hospital_department()
+        {
+            return View();
+        }
+
+        //医生类型
+        public IActionResult Hospital_Type()
+        {
+            return View();
+        }
+
+
+        //主治医生列表
+        public IActionResult Hospital_Doctor()
+        {
+            return View();
+        }
+        //医生详情表 
+        public IActionResult Hospital_DoctorDetails()
+        {
+            return View();
+        }
+
+        //诊断报告
+
+        public IActionResult Diagnostic_Report()
+        {
+            return View();
+        }
+
+
+        //------------------------------------------------------------------患者管理
+        //患者管理
+        public IActionResult Patient_Administration()
+        {
+            return View();
+        }
+        //患者信息
+        public IActionResult Patient_information()
+        {
+            return View();
+        }
+
+
+
+        //------------------------------------------------------------------审核管理
+        //审核管理-待审批 
+        public IActionResult Audit_management()
+        {
+            return View();
+        }
+        //审核管理-弹出层
+        public IActionResult Audit_managementpop()
+        {
+            return View();
+        }
+
+        //已通过
+        public IActionResult Approved()
+        {
+            return View();
+        }
+        //已通过-弹出层
+        public IActionResult Approvedpop()
+        {
+            return View();
+        }
+
+        //未通过
+        public IActionResult Refer()
+        {
+            return View();
+        }
+        //未通过-弹出层
+        public IActionResult Referpop()
+        {
+            return View();
+        }
+
+
+
+        // -------------------goo
+        //账号医生管理
+        public IActionResult Account()
+        {
+            return View();
+        }
+        //账号医生管理添加
+        public IActionResult Add()
+        {
+            return View();
+        }
+        //账号患者管理
+        public IActionResult Patient()
+        {
+            return View();
+        }
+        //账号患者修改
+        public IActionResult Upd()
+        {
+            return View();
+        }
+
+
+
+
     }
 }
